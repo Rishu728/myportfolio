e9c1b2e0a60d0e3c2d7865bd2a958caf3f67a376
Rishu2807 <rishukumari2807@gmail.com>
2023-09-03 22:06:06 +0530

    myportfolio_new_redesign1

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import NavBarContainer from '../src/Layout/Navbar/Navbar';
+
+function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Home-Hashtag404</title>
+            </Head>
+            <NavBarContainer />
+        </div>
+    );
+}
+export default Home;
